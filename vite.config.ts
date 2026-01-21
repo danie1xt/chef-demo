aa1a6e271a520a86fa527a098c73b899239c881c
danie1xt <danielle.lxtsz@gmail.com>
2026-01-21 01:01:29 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/chef-demo/', 
+    base: '/', 
 
     server: {
       port: 3000,
